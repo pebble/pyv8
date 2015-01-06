c3c97257aff58465dff695a1d90e76158074d481
Katharine Berry <katharine@getpebble.com>
2015-01-06 11:05:58 -0800

    Add support for harmony's Object.observe

--- src/PyV8.cpp
+++ src/PyV8.cpp
@@ -9,8 +9,16 @@
   #include "AST.h"
 #endif
 
+// So we can enable stuff that makes v8 support things we need.
+namespace v8 {
+  namespace internal {
+    extern bool FLAG_harmony_observation;
+  }
+}
+
 BOOST_PYTHON_MODULE(_PyV8)
 {
+  v8::internal::FLAG_harmony_observation = true;
   CJavascriptException::Expose();
   CWrapper::Expose(); 
   CContext::Expose();
